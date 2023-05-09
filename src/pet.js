255cac93284905ef554459c0a49e838530751085
Cristina Soare <kiti.soare@ymail.com>
2023-05-09 21:23:39 +0100

    added walk method on pet prototype

--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,24 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
-    this.name = name; 
-    this.age = 0; 
-    this.hunger = 0;
-    this.fitness = 10;
+  this.name = name; 
+  this.age = 0; 
+  this.hunger = 0;
+  this.fitness = MAXIMUM_FITNESS;
 }
 
 Pet.prototype.growUp = function() {
-    this.age += 1;
-    this.hunger +=5;
-    this.fitness -=3;
+  this.age += 1;
+  this.hunger +=5;
+  this.fitness -=3;
 };
+
+Pet.prototype.walk = function() {
+  if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+    this.fitness += 4;
+  } else {
+    this.fitness = MAXIMUM_FITNESS;
+  }
+}
 
 module.exports = Pet;
