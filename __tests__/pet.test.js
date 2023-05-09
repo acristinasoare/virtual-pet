55e800f9cf99b5c26f00a193c75191dd74769d84
Cristina Soare <kiti.soare@ymail.com>
2023-05-09 19:50:47 +0100

    wrote test for naming pet in constructor function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,9 @@ describe('constructor', () => {
   it('returns an object', () => {
     expect(new Pet('Fido')).toBeInstanceOf(Object);
   });
+
+  it('sets the name property', () => {
+    const pet = new Pet('Fido');
+    expect(pet.name).toEqual('Fido');
+  });
 }); 
