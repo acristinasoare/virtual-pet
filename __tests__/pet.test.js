c1e1284254daed696888d909ed75b004b6320d3e
Cristina Soare <kiti.soare@ymail.com>
2023-05-09 20:45:27 +0100

    edited growUp method test to include tests for hunger and fitness

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -14,6 +14,16 @@ describe('constructor', () => {
     const pet = new Pet('Fido');
     expect(pet.age).toEqual(0);
   });
+
+  it('has an initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.hunger).toEqual(0);
+  });
+
+  it('has an intial fitness of 10', () => {
+    const pet = new Pet('Fido');
+    expect(pet.fitness).toEqual(10);
+  })
 }); 
 
 describe('growUp', () => {
@@ -24,4 +34,20 @@ describe('growUp', () => {
 
     expect(pet.age).toEqual(1);
   });
+
+  it('increases hunger by 5 ', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.hunger).toEqual(5);
+  });
+
+  it('decreases fitness by 3', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.fitness).toEqual(7);
+  })
 });
